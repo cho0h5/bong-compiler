177cf64f52c3a7cb929bbe167b2a00bcc7b7f427
Youngho Cho <dominoyh5@gmail.com>
2023-11-28 10:46:11 +0900

    token reader to lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -3,15 +3,8 @@ use std::collections::VecDeque;
 use crate::parser::formatting::Tokens;
 use crate::parser::Node::Terminal;
 
-// token을 읽는 중에 발생할 수 있는 에러를 나타내는 struct입니다.
-// read_tokens(...) 함수는 parsing에 성공하면 Tokens를,
-// 실패하면 이 UnknownTokenError를 return합니다.
-// 이 struct는 어떤 키워드가 문제인지 &str형의 데이터를 담을 수 있습니다.
 pub struct UnknownTokenError<'a>(pub &'a str);
 
-// 토큰을 나타내는 enum입니다.
-// 구현의 간결함을 위해 terminal과 non-terminal을 함께 Token으로 정의하였습니다.
-// 문자열의 종료를 표현하기 위해 EOL도 추가하였습니다.
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 pub enum Token {
     // terminals
@@ -77,16 +70,7 @@ pub enum Token {
     CONTINUE_STMT,
 }
 
-// token을 인식하는 함수입니다.
-// 이 함수는 String을 받아 whitespace로 split한 후 token을 문자열 비교하여 인식합니다.
-// token 인식에 모두 성공하면 Tokens를 return하고, 실패하면 UnknownTokenError를 return합니다.
-// Tokens는 src/parser/formatting.rs에 정의되어있으며 그 내용은 다음과 같습니다.
-// pub struct Tokens(pub VecDeque<Node>);
-// Node는 tree를 나타내기 위한 자료형이며 src/parser/mod.rs에 정의되어있습니다.
-// VecDeque는 double-ended queue를 나타내는 rust의 기본 collection입니다.
-// read_tokens(...) 함수는 parsing된 token을 뒤에 추가하고, parser::parse(...) 함수는
-// token을 앞에서부터 읽기 때문에 VecDeque를 사용하였습니다.
-pub fn read_tokens(contents: &str) -> Result<Tokens, UnknownTokenError> {
+pub fn read_lexeme(contents: &str) -> Result<Tokens, UnknownTokenError> {
     let mut tokens = VecDeque::new();
 
     for word in contents.split_whitespace() {
@@ -116,13 +100,11 @@ pub fn read_tokens(contents: &str) -> Result<Tokens, UnknownTokenError> {
             "return" => Token::Return,
             "break" => Token::Break,
             "continue" => Token::Continue,
-            // token 인식을 실패하면 UnknownTokenError에 정보를 담아 return합니다.
             unknown_token => return Err(UnknownTokenError(unknown_token)),
         };
         tokens.push_back(Terminal(token));
     }
 
-    // token을 모두 인식하였다면 마지막에 EOL token을 추가하고 return합니다.
     tokens.push_back(Terminal(Token::EOL));
     Ok(Tokens(tokens))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,62 @@
-mod token_reader;
+mod lexer;
 mod parser;
 
-use std::process;
 use std::env;
 use std::fs;
+use std::process;
 
-use token_reader::UnknownTokenError;
+use lexer::UnknownTokenError;
 use parser::ParsingError;
 
-// 이 main함수는 총 4개의 과정을 거쳐 parsing tree를 생성합니다.
-// 1. 파일 이름 가져오기
-// 2. 파일 읽기
-// 3. token 인식하기
-// 4. parsing
-
 fn main() {
-    // 1. 입력받은 파일 이름이 있다면 filename에 저장합니다.
     let filename = match env::args().nth(1) {
         Some(filename) => {
             print!("\x1b[32m[1/4]\x1b[37m ");
             println!("File name: {}\n", filename);
             filename
-        },
+        }
         None => {
-            // 입력된 argument가 없다면 에러 내용을 출력하고 종료합니다.
             println!("\x1b[31m[1/4] error\x1b[37m: no input file");
             process::exit(1);
-        },
+        }
     };
 
-    // 2. 파일 읽기에 성공하면 raw_contents에 저장합니다.
     let raw_contents = match fs::read_to_string(filename) {
         Ok(contents) => {
             print!("\x1b[32m[2/4]\x1b[37m ");
             println!("File contents: \n{}\n", contents.trim());
             contents
-        },
+        }
         Err(_) => {
-            // 파일 읽는 중에 에러가 발생했다면 에러를 출력하고 종료합니다.
             println!("\x1b[31m[2/4] error\x1b[37m: something went wrong during reading file");
             process::exit(1);
-        },
+        }
     };
 
-    // 3. token을 인식하는데 성공하면 tokens의 배열을 tokens에 저장합니다.
-    //    token_reader::reader_token(...) 함수는 src/token_reader.rs 에 작성되어 있습니다.
-    let tokens = match token_reader::read_tokens(&raw_contents) {
+    let tokens = match lexer::read_lexeme(&raw_contents) {
         Ok(tokens) => {
             print!("\x1b[32m[3/4]\x1b[37m ");
             println!("Read tokens:\n{}\n", tokens);
             tokens
-        },
+        }
         Err(UnknownTokenError(token)) => {
-            // token을 인식하는 중 에러가 발생했다면 인식할 수 없는 키워드를 출력한 후 종료합니다.
             println!("\x1b[31m[3/4] error\x1b[37m: unknown token: {}", token);
             process::exit(1);
-        },
+        }
     };
 
-    // 4. 입력된 tokens를 parsing하고 parse tree를 생성한 후 출력합니다.
-    //    parser::parse(...) 함수는 src/parser/mod.rs 에 작성되어 있습니다.
     match parser::parse(tokens) {
         Ok(tree) => {
             print!("\x1b[32m[4/4]\x1b[37m ");
             println!("Parse tree:\n{}", tree);
             println!("Accepted!");
             tree
-        },
+        }
         Err(ParsingError(expected, found)) => {
-            // parsing에 실패했다면 어느 token을 parsing하다가 실패하였는지, 어떤 token이
-            // expected되는지 출력합니다.
             println!("\x1b[31m[4/4] error\x1b[37m: parsing error");
             println!("\texpected: {:?}", expected);
             println!("\tbut found: {:?}", found);
             process::exit(1);
-        },
+        }
     };
 }
