c17538511759300892e7a925f905dbb63b81e166
Youngho Cho <dominoyh5@gmail.com>
2023-11-28 13:49:43 +0900

    특수문자 lexer ok

--- src/lexer.rs
+++ src/lexer.rs
@@ -76,40 +76,169 @@ pub fn read_lexeme(contents: &str) -> Result<Tokens, UnknownTokenError> {
     let mut tokens = VecDeque::new();
 
     let mut iter = contents.chars();
+    let mut char = iter.next();
+    loop {
+        let token = match char {
+            Some('[') => {
+                char = iter.next();
+                Token::Lbracket
+            }
+            Some(']') => {
+                char = iter.next();
 
-    for word in contents.split_whitespace() {
-        let token = match word {
-            "int" => Token::Int,
-            "void" => Token::Void,
-            "lbracket" => Token::Lbracket,
-            "int_lit" => Token::IntLit, // TODO : 숫자로 시작, 또는 작은 따옴표
-            "rbracket" => Token::Rbracket,
-            "star" => Token::Star,
-            "lbrace" => Token::Lbrace,
-            "rbrace" => Token::Rbrace,
-            "semicolon" => Token::Semicolon,
-            "identifier" => Token::Identifier, // TODO : 문자로 시작
-            "lparen" => Token::Lparen,
-            "rparen" => Token::Rparen,
-            "comma" => Token::Comma,
-            "string_lit" => Token::StringLit, // TODO : 따옴표 사이
-            "log_op" => Token::LogOp,         // TODO:: || &&
-            "rel_op" => Token::RelOp,         // TODO : == != < <= > >=
-            "add_op" => Token::AddOp,         // TODO : | ^
-            "add_minus" => Token::AddMinus,
-            "mul_op" => Token::MulOp, // TODO : * / % << >> &
-            "and" => Token::And,
-            "unary_op" => Token::UnaryOp, // TODO : ! ~
-            "assign_op" => Token::AssignOp,
-            "if" => Token::If,
-            "while" => Token::While,
-            "return" => Token::Return,
-            "break" => Token::Break,
-            "continue" => Token::Continue,
-            unknown_token => return Err(UnknownTokenError(unknown_token)),
+                Token::Rbracket
+            }
+            Some('*') => {
+                char = iter.next();
+
+                Token::Star
+            }
+            Some('{') => {
+                char = iter.next();
+
+                Token::Lbrace
+            }
+            Some('}') => {
+                char = iter.next();
+
+                Token::Rbrace
+            }
+            Some(';') => {
+                char = iter.next();
+
+                Token::Semicolon
+            }
+            Some('(') => {
+                char = iter.next();
+                Token::Lparen
+            }
+            Some(')') => {
+                char = iter.next();
+                Token::Rparen
+            }
+            Some(',') => {
+                char = iter.next();
+                Token::Comma
+            }
+            Some('+') => {
+                char = iter.next();
+                Token::AddMinus
+            }
+            Some('-') => {
+                char = iter.next();
+                Token::AddMinus
+            }
+            Some('~') => {
+                char = iter.next();
+                Token::UnaryOp
+            }
+            Some('/') => {
+                char = iter.next();
+                Token::MulOp
+            }
+            Some('%') => {
+                char = iter.next();
+                Token::MulOp
+            }
+            Some('^') => {
+                char = iter.next();
+                Token::AddOp
+            }
+            Some('=') => {
+                char = iter.next();
+                match char {
+                    Some('=') => {
+                        char = iter.next();
+                        Token::RelOp
+                    }
+                    _ => Token::AssignOp,
+                }
+            }
+            Some('&') => {
+                char = iter.next();
+                match char {
+                    Some('&') => {
+                        char = iter.next();
+                        Token::LogOp
+                    }
+                    _ => Token::And,
+                }
+            }
+            Some('|') => {
+                char = iter.next();
+                match char {
+                    Some('|') => {
+                        char = iter.next();
+                        Token::LogOp
+                    }
+                    _ => Token::AddOp,
+                }
+            }
+            Some('!') => {
+                char = iter.next();
+                match char {
+                    Some('=') => {
+                        char = iter.next();
+                        Token::RelOp
+                    }
+                    _ => Token::UnaryOp,
+                }
+            }
+            Some('<') => {
+                char = iter.next();
+                match char {
+                    Some('=') => {
+                        char = iter.next();
+                        Token::RelOp
+                    }
+                    Some('<') => {
+                        char = iter.next();
+                        Token::MulOp
+                    }
+                    _ => Token::RelOp,
+                }
+            }
+            Some('>') => {
+                char = iter.next();
+                match char {
+                    Some('=') => {
+                        char = iter.next();
+                        Token::RelOp
+                    }
+                    Some('>') => {
+                        char = iter.next();
+                        Token::MulOp
+                    }
+                    _ => Token::RelOp,
+                }
+            }
+            Some(c) if c.is_ascii_whitespace() => {
+                char = iter.next();
+                continue;
+            }
+            Some(unexpected_char) => panic!("unexpected_char: {}", unexpected_char),
+            _ => break,
         };
         tokens.push_back(Terminal(token));
     }
+
+    //    for word in contents.split_whitespace() {
+    //        let token = match word {
+    //            "int" => Token::Int,
+    //            "void" => Token::Void,
+    //            "if" => Token::If,
+    //            "while" => Token::While,
+    //            "return" => Token::Return,
+    //            "break" => Token::Break,
+    //            "continue" => Token::Continue,
+    //
+    //            "int_lit" => Token::IntLit, // TODO : 숫자로 시작, 또는 작은 따옴표
+    //            "identifier" => Token::Identifier, // TODO : 문자로 시작
+    //            "string_lit" => Token::StringLit, // TODO : 따옴표 사이
+    //            unknown_token => return Err(UnknownTokenError(unknown_token)),
+    //        };
+    //        tokens.push_back(Terminal(token));
+    //    }
 
     tokens.push_back(Terminal(Token::EOL));
     Ok(Tokens(tokens))
