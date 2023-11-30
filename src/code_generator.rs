e3b15a467225983a20e52f580606d020f7caabec
Youngho Cho <dominoyh5@gmail.com>
2023-11-30 16:19:10 +0900

    logiical expr Ok?

--- src/code_generator.rs
+++ src/code_generator.rs
@@ -69,7 +69,7 @@ fn generate_function_decl(
     }
 
     if let Node::NonTerminal(Token::BLOCK, children) = &children[3] {
-        code.extend(generate_block(symbol_table, children));
+        code.extend(generate_block(children));
     }
 
     code.push(Box::new(IFormat::new(
@@ -139,30 +139,27 @@ fn generate_parameter_decl(count: i16) -> Vec<Box<dyn Instruction>> {
     code
 }
 
-fn generate_block(symbol_table: &SymbolTable, children: &Vec<Node>) -> Vec<Box<dyn Instruction>> {
+fn generate_block(children: &Vec<Node>) -> Vec<Box<dyn Instruction>> {
     let mut code: Vec<Box<dyn Instruction>> = Vec::new();
 
     if let Node::NonTerminal(Token::STATEMENT_LIST, children) = &children[1] {
-        code.extend(generate_statement_list(symbol_table, children));
+        code.extend(generate_statement_list(children));
     }
 
     code
 }
 
-fn generate_statement_list(
-    symbol_table: &SymbolTable,
-    children: &Vec<Node>,
-) -> Vec<Box<dyn Instruction>> {
+fn generate_statement_list(children: &Vec<Node>) -> Vec<Box<dyn Instruction>> {
     let mut code: Vec<Box<dyn Instruction>> = Vec::new();
 
     if !children.is_empty() {
         if let Node::NonTerminal(Token::STATEMENT, children) = &children[0] {
-            code.extend(generate_statement(symbol_table, children));
+            code.extend(generate_statement(children));
         }
 
         if children.len() >= 3 {
             if let Node::NonTerminal(Token::STATEMENT_LIST, children) = &children[2] {
-                code.extend(generate_statement_list(symbol_table, children));
+                code.extend(generate_statement_list(children));
             }
         }
     }
@@ -170,34 +167,32 @@ fn generate_statement_list(
     code
 }
 
-fn generate_statement(
-    symbol_table: &SymbolTable,
-    children: &Vec<Node>,
-) -> Vec<Box<dyn Instruction>> {
+fn generate_statement(children: &[Node]) -> Vec<Box<dyn Instruction>> {
     let mut code: Vec<Box<dyn Instruction>> = Vec::new();
 
     match &children[0] {
         Node::NonTerminal(Token::ASSIGNMENT, children) => {
-            code.extend(generate_assignment(symbol_table, children));
+            code.extend(generate_assignment(children));
         }
         Node::NonTerminal(Token::VAR_DECL, _) => (),
         Node::NonTerminal(Token::RETURN_STMT, children) => {
-            code.extend(generate_return_stmt(symbol_table, children));
+            code.extend(generate_return_stmt(children));
         }
         Node::NonTerminal(Token::BREAK_STMT, children) => {
-            code.extend(generate_break_stmt(symbol_table, children));
+            code.extend(generate_break_stmt(children));
         }
         Node::NonTerminal(Token::CONTINUE_STMT, children) => {
-            code.extend(generate_continue_stmt(symbol_table, children));
+            code.extend(generate_continue_stmt(children));
         }
         Node::NonTerminal(Token::IF_STMT, children) => {
-            code.extend(generate_if_stmt(symbol_table, children));
+            code.extend(generate_if_stmt(children));
         }
         Node::NonTerminal(Token::WHILE_STMT, children) => {
-            code.extend(generate_while_stmt(symbol_table, children));
+            code.extend(generate_while_stmt(children));
         }
         Node::NonTerminal(Token::EXPRESSION, children) => {
-            code.extend(generate_expression(symbol_table, children));
+            let mut offset = 0;
+            code.extend(generate_expression(children, &mut offset));
         }
         node => panic!("unvalid parse tree: {:?}", node),
     }
@@ -205,10 +200,7 @@ fn generate_statement(
     code
 }
 
-fn generate_assignment(
-    symbol_table: &SymbolTable,
-    children: &Vec<Node>,
-) -> Vec<Box<dyn Instruction>> {
+fn generate_assignment(children: &Vec<Node>) -> Vec<Box<dyn Instruction>> {
     let mut code: Vec<Box<dyn Instruction>> = Vec::new();
 
     for child in children {
@@ -218,10 +210,7 @@ fn generate_assignment(
     code
 }
 
-fn generate_return_stmt(
-    symbol_table: &SymbolTable,
-    children: &Vec<Node>,
-) -> Vec<Box<dyn Instruction>> {
+fn generate_return_stmt(children: &Vec<Node>) -> Vec<Box<dyn Instruction>> {
     let mut code: Vec<Box<dyn Instruction>> = Vec::new();
 
     for child in children {
@@ -231,10 +220,7 @@ fn generate_return_stmt(
     code
 }
 
-fn generate_break_stmt(
-    symbol_table: &SymbolTable,
-    children: &Vec<Node>,
-) -> Vec<Box<dyn Instruction>> {
+fn generate_break_stmt(children: &Vec<Node>) -> Vec<Box<dyn Instruction>> {
     let mut code: Vec<Box<dyn Instruction>> = Vec::new();
 
     for child in children {
@@ -244,10 +230,7 @@ fn generate_break_stmt(
     code
 }
 
-fn generate_continue_stmt(
-    symbol_table: &SymbolTable,
-    children: &Vec<Node>,
-) -> Vec<Box<dyn Instruction>> {
+fn generate_continue_stmt(children: &Vec<Node>) -> Vec<Box<dyn Instruction>> {
     let mut code: Vec<Box<dyn Instruction>> = Vec::new();
 
     for child in children {
@@ -257,7 +240,7 @@ fn generate_continue_stmt(
     code
 }
 
-fn generate_if_stmt(symbol_table: &SymbolTable, children: &Vec<Node>) -> Vec<Box<dyn Instruction>> {
+fn generate_if_stmt(children: &Vec<Node>) -> Vec<Box<dyn Instruction>> {
     let mut code: Vec<Box<dyn Instruction>> = Vec::new();
 
     for child in children {
@@ -267,10 +250,7 @@ fn generate_if_stmt(symbol_table: &SymbolTable, children: &Vec<Node>) -> Vec<Box
     code
 }
 
-fn generate_while_stmt(
-    symbol_table: &SymbolTable,
-    children: &Vec<Node>,
-) -> Vec<Box<dyn Instruction>> {
+fn generate_while_stmt(children: &Vec<Node>) -> Vec<Box<dyn Instruction>> {
     let mut code: Vec<Box<dyn Instruction>> = Vec::new();
 
     for child in children {
@@ -280,10 +260,207 @@ fn generate_while_stmt(
     code
 }
 
-fn generate_expression(
-    symbol_table: &SymbolTable,
-    children: &Vec<Node>,
+fn generate_expression(children: &[Node], offset: &mut i16) -> Vec<Box<dyn Instruction>> {
+    let mut code: Vec<Box<dyn Instruction>> = Vec::new();
+    let this_offset = *offset;
+    let child_offset = *offset + 4;
+    *offset += 4;
+
+    if let Node::NonTerminal(Token::LOGICAL_EXPR, children) = &children[0] {
+        code.extend(generate_logical_expr(children, offset))
+    }
+
+    code.push(Box::new(IFormat::new(
+        OpCode::Lui,
+        RegisterName::Zero,
+        RegisterName::T0,
+        4096,
+    )));
+    code.push(Box::new(IFormat::new(
+        OpCode::Lw,
+        RegisterName::T0,
+        RegisterName::T1,
+        child_offset,
+    )));
+    code.push(Box::new(IFormat::new(
+        OpCode::Sw,
+        RegisterName::T0,
+        RegisterName::T1,
+        this_offset,
+    )));
+
+    code
+}
+
+fn generate_logical_expr(children: &[Node], mut offset: &mut i16) -> Vec<Box<dyn Instruction>> {
+    let mut code: Vec<Box<dyn Instruction>> = Vec::new();
+    let this_offset = *offset;
+    *offset += 4;
+    let child_offset = *offset;
+
+    match &children[0] {
+        Node::NonTerminal(Token::LOGICAL_EXPR, children2) => {
+            code.extend(generate_logical_expr(children2, offset));
+
+            let child2_offset = *offset;
+            if let Node::NonTerminal(Token::LOGICAL_EXPR, children) = &children[2] {
+                code.extend(generate_relational_expr(children, offset));
+            }
+            code.extend(generate_load_2children_to_t1_t2(
+                child_offset,
+                child2_offset,
+            ));
+
+            match children[1] {
+                // TODO bitwise or 말고 logical or로 바꿔야함
+                Node::Terminal(Token::LogOp(LogicalOperator::Or)) => {
+                    code.push(Box::new(RFormat::new(
+                        Funct::Or,
+                        RegisterName::T1,
+                        RegisterName::T2,
+                        RegisterName::T1,
+                        0,
+                    )));
+                }
+                // TODO bitwise and 말고 logical and로 바꿔야함
+                Node::Terminal(Token::LogOp(LogicalOperator::And)) => {
+                    code.push(Box::new(RFormat::new(
+                        Funct::And,
+                        RegisterName::T1,
+                        RegisterName::T2,
+                        RegisterName::T1,
+                        0,
+                    )));
+                }
+                _ => panic!("no way"),
+            }
+            code.extend(generate_store_t1_to_offset(this_offset));
+        }
+        Node::NonTerminal(Token::RELATIONAL_EXPR, children) => {
+            code.extend(generate_relational_expr(children, offset));
+            code.extend(generate_move(this_offset, child_offset));
+        }
+        _ => (),
+    }
+
+    code
+}
+
+fn generate_load_2children_to_t1_t2(
+    child1_offset: i16,
+    child2_offset: i16,
 ) -> Vec<Box<dyn Instruction>> {
+    let mut code: Vec<Box<dyn Instruction>> = Vec::new();
+
+    code.push(Box::new(IFormat::new(
+        OpCode::Lui,
+        RegisterName::Zero,
+        RegisterName::T0,
+        4096,
+    )));
+    code.push(Box::new(IFormat::new(
+        OpCode::Lw,
+        RegisterName::T0,
+        RegisterName::T1,
+        child1_offset,
+    )));
+    code.push(Box::new(IFormat::new(
+        OpCode::Lw,
+        RegisterName::T0,
+        RegisterName::T2,
+        child2_offset,
+    )));
+
+    code
+}
+
+fn generate_store_t1_to_offset(offset: i16) -> Vec<Box<dyn Instruction>> {
+    let mut code: Vec<Box<dyn Instruction>> = Vec::new();
+
+    code.push(Box::new(IFormat::new(
+        OpCode::Lui,
+        RegisterName::Zero,
+        RegisterName::T0,
+        4096,
+    )));
+
+    code.push(Box::new(IFormat::new(
+        OpCode::Sw,
+        RegisterName::T0,
+        RegisterName::T1,
+        offset,
+    )));
+
+    code
+}
+
+fn generate_move(this_offset: i16, child_offset: i16) -> Vec<Box<dyn Instruction>> {
+    let mut code: Vec<Box<dyn Instruction>> = Vec::new();
+
+    code.push(Box::new(IFormat::new(
+        OpCode::Lui,
+        RegisterName::Zero,
+        RegisterName::T0,
+        4096,
+    )));
+    code.push(Box::new(IFormat::new(
+        OpCode::Lw,
+        RegisterName::T0,
+        RegisterName::T1,
+        child_offset,
+    )));
+    code.push(Box::new(IFormat::new(
+        OpCode::Sw,
+        RegisterName::T0,
+        RegisterName::T1,
+        this_offset,
+    )));
+
+    code
+}
+
+fn generate_relational_expr(children: &[Node], offset: &mut i16) -> Vec<Box<dyn Instruction>> {
+    let mut code: Vec<Box<dyn Instruction>> = Vec::new();
+    let this_offset = *offset;
+    *offset += 4;
+    let child_offset = *offset;
+
+    // match &children[0] {}
+
+    code
+}
+
+fn generate_additive_expr(children: &[Node], offset: &mut i16) -> Vec<Box<dyn Instruction>> {
+    let mut code: Vec<Box<dyn Instruction>> = Vec::new();
+
+    for child in children {
+        traverse_debug(child);
+    }
+
+    code
+}
+
+fn generate_multiplicative_expr(children: &[Node], offset: &mut i16) -> Vec<Box<dyn Instruction>> {
+    let mut code: Vec<Box<dyn Instruction>> = Vec::new();
+
+    for child in children {
+        traverse_debug(child);
+    }
+
+    code
+}
+
+fn generate_unary_expr(children: &[Node], offset: &mut i16) -> Vec<Box<dyn Instruction>> {
+    let mut code: Vec<Box<dyn Instruction>> = Vec::new();
+
+    for child in children {
+        traverse_debug(child);
+    }
+
+    code
+}
+
+fn generate_primary_expr(children: &[Node], offset: &mut i16) -> Vec<Box<dyn Instruction>> {
     let mut code: Vec<Box<dyn Instruction>> = Vec::new();
 
     for child in children {
